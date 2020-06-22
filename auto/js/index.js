c15971d7c93a4c72659de160ed839a3e46430c89
Alessio Ricci <ale.ricci@hispeed.ch>
2020-06-22 13:12:50 +0200

    JS

---
+++ auto/js/index.js
@@ -0,0 +1,9 @@
+console.log('JS eingebunden');
+console.warn('Warnung: test');
+console.error('Error: error');
+
+MeineFunktion('Das ist eine Funktion');
+
+function MeineFunktion(MeineVariable){
+    console.log('Funktion: ' + MeineVariable);
+}
