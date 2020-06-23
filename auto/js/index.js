bb5c3a0114ac7e5428deb89fa9d175993aec2d7e
Alessio Ricci <ale.ricci@hispeed.ch>
2020-06-23 11:16:26 +0200

    JSON

--- auto/js/index.js
+++ auto/js/index.js
@@ -1,43 +1,49 @@
 $(function(){
-    $('.modal').modal();  
+    $.getJSON("/auto/data/auto.json", function (data) {
+            var mtpl = $('#myTemplate').html();
+            var html = Mustache.render(mtpl, data);
+            $('tbody').append(html);
 
+            $('.modal').modal(); 
 
-    $('.edit').click(function (e) { 
-        e.preventDefault();
-        var id = $(this).parent().attr('data-id');
-        $('.modal').modal();
-        $('#modat_titel').html('Auto editieren von ' + id);
-        $('#modat_inhalt').load('/auto/sites/formular.html');
-        var mymodal = M.Modal.getInstance($('.modal'));
-        mymodal.open();
-    });
+            $('.edit').click(function (e) { 
+                e.preventDefault();
+                var id = $(this).parent().attr('data-id');
+                $('.modal').modal();
+                $('#modat_titel').html('Auto editieren von ' + id);
+                $('#modat_inhalt').load('/auto/sites/formular.html');
+                var mymodal = M.Modal.getInstance($('.modal'));
+                mymodal.open();
+            });
 
-    $('.tanken').click(function (e) { 
-        e.preventDefault();
-        var id = $(this).parent().attr('data-id');
-        $('.modal').modal();
-        $('#modat_titel').html('Auto tanken von ' + id);
-        $('#modat_inhalt').load('/auto/sites/formular.html');
-        var mymodal = M.Modal.getInstance($('.modal'));
-        mymodal.open();
-    });
+            $('.tanken').click(function (e) { 
+                e.preventDefault();
+                var id = $(this).parent().attr('data-id');
+                $('.modal').modal();
+                $('#modat_titel').html('Auto tanken von ' + id);
+                $('#modat_inhalt').load('/auto/sites/formular.html');
+                var mymodal = M.Modal.getInstance($('.modal'));
+                mymodal.open();
+            });
 
-    $('.delete').click(function (e) { 
-        e.preventDefault();
-        var id = $(this).parent().attr('data-id');
-        $('.modal').modal();
-        $('#modat_titel').html('Auto löschen von ' + id);
-        $('#modat_inhalt').load('/auto/sites/formular.html');
-        var mymodal = M.Modal.getInstance($('.modal'));
-        mymodal.open();
-    });
+            $('.delete').click(function (e) { 
+                e.preventDefault();
+                var id = $(this).parent().attr('data-id');
+                $('.modal').modal();
+                $('#modat_titel').html('Auto löschen von ' + id);
+                $('#modat_inhalt').load('/auto/sites/formular.html');
+                var mymodal = M.Modal.getInstance($('.modal'));
+                mymodal.open();
+            });
 
-    $('.new').click(function (e) { 
-        e.preventDefault();
-        $('#modat_titel').html('Auto hinzufügen');
-        $('#modat_inhalt').load('/auto/sites/formular.html');
-        var mymodal = M.Modal.getInstance($('.modal'));
-        mymodal.open();
-    });
+            $('.new').click(function (e) { 
+                e.preventDefault();
+                $('#modat_titel').html('Auto hinzufügen');
+                $('#modat_inhalt').load('/auto/sites/formular.html');
+                var mymodal = M.Modal.getInstance($('.modal'));
+                mymodal.open();
+            });
+        }
+    );
 
 });
---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+    $.getJSON("json/data.json", function (data) {
+            console.log(data.person);
+            data.error.forEach(element => {
+                M.toast(
+                    {
+                        html: element.meldung, classes: 'rounded red'
+                    }
+                )
+            });
+            var mtpl = $('#myTemplate').html();
+            var html = Mustache.render(mtpl, data);
+            $('main').append(html);
+            $('.edit').click(function (e) {
+                e.preventDefault();
+                console.log('edit click');
+            });
+        }
+    );
+});
